3dbac5b80d79e2003ec56aba91761405295cad71
Erik Danielsson <danielsson.erik.0@gmail.com>
2023-05-12 21:24:25 +0200

    Add some comments

--- 5gorilla/solution.py
+++ 5gorilla/solution.py
@@ -1,5 +1,6 @@
 #! /bin/env python
 import sys
+import time
 
 import numpy as np
 
@@ -20,15 +21,18 @@ def read_input():
 
 def align(fst, snd, chars, costs, delta):
     # Align two string using the Needleman-Wunsch algorithm
+    # Convert to a numerical representation
     num_fst = [chars[c] for c in fst]
     num_snd = [chars[c] for c in snd]
     n = len(fst)
     m = len(snd)
+    # Initialize the array with no-alignment costs
     opt = np.zeros((n + 1, m + 1))
     opt[:, 0] = [i * delta for i in range(n + 1)]
     opt[0, :] = [i * delta for i in range(m + 1)]
     min_val = float("inf")
-    # Compute the opt matrix
+
+    # Now compute the opt matrix
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             c1 = num_fst[i - 1]
@@ -39,7 +43,7 @@ def align(fst, snd, chars, costs, delta):
             delete = delta + opt[i, j - 1]
             opt[i, j] = max(match, insert, delete)
 
-    # Now compute the alignments
+    # Reconstruct the alignment by walking backwards from lower right corner of the matrix
     fst_align = []
     snd_align = []
     i = n
@@ -61,15 +65,28 @@ def align(fst, snd, chars, costs, delta):
             snd_align.append(snd[j - 1])
             j -= 1
 
+    # Return the aligned strings in the correct order
     return "".join(reversed(fst_align)), "".join(reversed(snd_align))
 
 
 def main():
+    tic = time.perf_counter()
     chars, costs, queries = read_input()
+    toc = time.perf_counter()
+    print(f"Reading time: {toc - tic}", file=sys.stderr)
     delta = -4
     alignments = []
+
+    tot_alignment_time = 0
     for fst, snd in queries:
+        tic = time.perf_counter()
         alignments.append(align(fst, snd, chars, costs, delta))
+        toc = time.perf_counter()
+        tot_alignment_time += toc - tic
+    print(
+        f"Average alignment time: {tot_alignment_time / len(queries)}", file=sys.stderr
+    )
+
     for fst_align, snd_align in alignments:
         print(f"{fst_align} {snd_align}")
 
